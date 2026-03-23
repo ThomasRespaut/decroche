1ce2daade643f4d741571a8d2ea8f1b928e1f9d8
Thomas Respaut <thomas.respaut@edu.devinci.fr>
2026-03-23 09:58:16 +0100

    phone

--- calls/urls.py
+++ calls/urls.py
@@ -4,4 +4,8 @@ from .views import twilio_incoming_call, twilio_call_status_callback
 urlpatterns = [
     path("twilio/incoming/", twilio_incoming_call, name="twilio_incoming_call"),
     path("twilio/status/", twilio_call_status_callback, name="twilio_call_status_callback"),
+
+    # alias temporaires
+    path("voice", twilio_incoming_call),
+    path("status", twilio_call_status_callback),
 ]
